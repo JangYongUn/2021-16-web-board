46664681c3c6473d1cc007b3951f86bcca8bb35d
JangYongUn <sm970413@naver.com>
2021-02-04 14:45:08 +0900

    .......

--- modules/dist/logger.dev.js
+++ modules/dist/logger.dev.js
@@ -4,4 +4,23 @@ var morgan = require('morgan');
 
 var rfs = require('rotating-file-stream');
 
-module.ex;
+var path = require('path');
+
+var moment = require('moment');
+
+var streamCommon = rfs.createStream(moment().format('YYYYMMDD') + '.log', {
+  interval: "1d",
+  path: path.join(__dirname, '../logs')
+});
+var streamCombined = rfs.createStream(moment().format('YYYYMMDD') + '_c.log', {
+  interval: "1d",
+  path: path.join(__dirname, '../logs')
+});
+
+module.exports = function (method) {
+  if (method == 'common') return morgan(method, {
+    stream: streamCommon
+  });else return morgan(method, {
+    stream: streamCombined
+  });
+};
--- modules/logger.js
+++ modules/logger.js
@@ -2,18 +2,18 @@ const morgan = require('morgan');
 const rfs = require('rotating-file-stream');
 const path = require('path');
 const moment = require('moment');
-// const logFile = path.join(__dirname, '../log', moment().format('YYYYMMDD')+'.log');
 
-const stream = rfs.createStream(/* "file.log" */ moment().format('YYYYMMDD')+'.log', {
+const streamCommon = rfs.createStream(moment().format('YYYYMMDD')+'.log', { 
 	interval: "1d",
-	path: path.join(__dirname, '../log');
+	path: path.join(__dirname, '../logs')
 });
 
-const stream = rfs.createStream(/* "file.log" */ moment().format('YYYYMMDD')+'_c.log', {
+const streamCombined = rfs.createStream(moment().format('YYYYMMDD')+'_c.log', { 
 	interval: "1d",
-	path: path.join(__dirname, '../log');
+	path: path.join(__dirname, '../logs')
 });
 
-module.exports = () => {
-
+module.exports = (method) => {
+	if(method == 'common') return morgan(method, { stream: streamCommon });
+	else return morgan(method, { stream: streamCombined });
 };
