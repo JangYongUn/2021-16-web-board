a8084f66e497ba4050b77313d4807fc670b7f6d2
JangYongUn <sm970413@naver.com>
2021-02-05 18:33:11 +0900

    .......

--- routes/gallery-route.js
+++ routes/gallery-route.js
@@ -3,7 +3,7 @@ const moment = require('moment');
 const path = require('path');
 const fs = require('fs-extra');
 const ip = require('request-ip');
-const { upload, imgExt } = require('../modules/multer');
+const { uploadImg, imgExt } = require('../modules/multer');
 const { pool, sqlGen: sql } = require('../modules/mysql-pool');
 const { err, alert, extName, srcPath, realPath } = require('../modules/util');
 const pagers = require('../modules/pager');
@@ -18,7 +18,12 @@ const pugs = {
 }
 
 router.get('/create', isUser, (req, res, next) => {
-	
+	res.render('gallery/create', pugs);
+});
+
+router.post('/save', isUser, uploadImg.array('upfile', 10), async (req, res, next) => {
+	console.log(req.banExt)
+	res.json(req.files);
 });
 
 
